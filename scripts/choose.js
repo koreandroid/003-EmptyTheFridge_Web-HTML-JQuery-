beb66c4c2781caf5f38f127bc2b6d5469ea2bf08
JAESEONG-L <dlwotjd116@snu.ac.kr>
2024-09-25 01:15:01 +0900

    feat: 레시피 결정 모달 추가 및 연결

--- scripts/choose.js
+++ scripts/choose.js
@@ -1,9 +1,9 @@
+/**
+ * < 페이지 >
+ */
 let recipeIdsForMainIngredient;             // 주재료 먼저 선택하기
 let recipeIdsForSecondaryIngredient;        // 부재료 먼저 선택하기
 
-/*
- * 페이지
- */
 {
     const $ = jQuery;
 
@@ -31,9 +31,11 @@ let recipeIdsForSecondaryIngredient;        // 부재료 먼저 선택하기
     });
 }
 
-/*
- * 주재료 선택 모달
+/**
+ * < 주재료 선택 모달 >
  */
+let recipeIds;
+
 function buildIngredientHtmlString(ingredientCount, irdntNm) {
     return `<input type="radio" id="ingredient_${ingredientCount}" class="btn-check" name="ingredient" autocomplete="off" />` +
     `<label class="btn btn-outline-primary" for="ingredient_${ingredientCount}">${irdntNm}</label>`;
@@ -43,7 +45,7 @@ function setupMainIngredientPickModal() {
     const mainIngredientPickModal = document.getElementById('main_ingredient_pick_modal');
 
     // 다음 버튼을 클릭했을 때
-    mainIngredientPickModal.addEventListener('show.bs.modal', event => {        // This event fires immediately when the show instance method is called.
+    mainIngredientPickModal.addEventListener('show.bs.modal', () => {       // This event fires immediately when the show instance method is called.
         const modalBody = mainIngredientPickModal.querySelector('.modal-body');
 
         let ingredientCount = 0;
@@ -55,19 +57,30 @@ function setupMainIngredientPickModal() {
             }
         }
 
-        if (ingredientCount == 0) {
+        if (ingredientCount) {
+            [...modalBody.getElementsByTagName('input')].forEach(
+                element => element.addEventListener('change', event => {
+                    const ingredient = event.target.nextSibling.textContent;
+
+                    recipeIds = recipeIdsForMainIngredient[ingredient];
+
+                    document.getElementById('main_pick_modal_next').setAttribute('data-bs-ingredient', ingredient);
+                    document.getElementById('main_pick_modal_next').disabled = false;
+                })
+            );
+        } else {
             modalBody.appendChild(document.createElement('p'))
                 .textContent = '검색된 재료가 없습니다ㅠㅠ';
-            mainIngredientPickModal.querySelector('#main_pick_modal_next').disabled = true;
         }
     });
-    mainIngredientPickModal.addEventListener('hidden.bs.modal', event => {      // This event is fired when the modal has finished being hidden from the user (will wait for CSS transitions to complete).
+    mainIngredientPickModal.addEventListener('hidden.bs.modal', () => {     // This event is fired when the modal has finished being hidden from the user (will wait for CSS transitions to complete).
         const modalBody = mainIngredientPickModal.querySelector('.modal-body');
         while (modalBody.firstChild)
         {
             modalBody.removeChild(modalBody.firstChild);
         }
-        mainIngredientPickModal.querySelector('#main_pick_modal_next').disabled = false;
+
+        document.getElementById('main_pick_modal_next').disabled = true;
     });
 }
 
@@ -75,4 +88,26 @@ function setupMainIngredientPickModal() {
     const $ = jQuery;
 
     $(document).ready(setupMainIngredientPickModal);
+}
+
+/**
+ * < 레시피 결정 모달 >
+ */
+
+function setupRecipeDecideWithMainModal() {
+    const recipeDecideWithMainModal = document.getElementById('recipe_decide_with_main_modal');
+
+    // 주재료 선택 모달(#main_ingredient_pick_modal)에서 다음 버튼을 클릭했을 때
+    recipeDecideWithMainModal.addEventListener('show.bs.modal', event => {
+        recipeDecideWithMainModal.querySelector('#decide_with_main_modal_label>label').textContent = event.relatedTarget.getAttribute('data-bs-ingredient');
+    });
+    recipeDecideWithMainModal.addEventListener('hidden.bs.modal', () => {
+        recipeDecideWithMainModal.querySelector('#decide_with_main_modal_label>label').textContent = '';
+    });
+}
+
+{
+    const $ = jQuery;
+
+    $(document).ready(setupRecipeDecideWithMainModal);
 }
