66784910b4d59d3936d0f12fe4ad513235b381de
koreandroid <dlwotjd116@snu.ac.kr>
2024-10-24 15:56:33 +0900

    feat: 십자말풀이 board 디자인 완성 및 칸 활성화 로직 추가

--- scripts/crossword.js
+++ scripts/crossword.js
@@ -1,16 +1,58 @@
 let board;
+let stageCount = 0;
 
 const inputList = [];
 
-function initBoard() {
-    const cellElements = board.getElementsByTagName('td');
-    for (el of cellElements) {
-        inputList.push(el.appendChild(document.createElement('input')));
+function setBoard(wordList) {
+    const tdElements = board.getElementsByTagName('td');
+    for (tdEl of tdElements) {
+        const textEl = document.createElement('input');
+        textEl.setAttribute('type', 'text');
+        textEl.setAttribute('maxlength', '1');
+        inputList.push(tdEl.appendChild(textEl));
+    }
+
+    const usingGrids = new Set();
+    for (const word of wordList) {
+        word.gridList.forEach(grid => usingGrids.add(grid));
+    }
+
+    // 칸 활성화하기
+    for (const grid of usingGrids) {
+        const index = 20 * (grid[0] - 1) + grid[1] - 1;
+        tdElements.item(index).setAttribute('class', `on bd-${stageCount % 2}`);
+
+        inputList[index].style.setProperty('pointer-events', 'auto');
     }
 }
 
+function getPuzzleList() {
+    let result = null;
+    jQuery.ajax({
+        async: false,
+        url: 'https://gist.githubusercontent.com/koreandroid/5766402dc38b48c27d135cf3959b369d/raw/9218f90376035bd5b46df36507ae9dc40f3270a3/crosswordPuzzleList.json',
+        dataType: 'json',
+        success: function(response) {
+            result = response.puzzleList;
+        },
+        error: function(xhr, status, error) {
+            console.log('Error:', error);
+        }
+    });
+
+    return result;
+}
+
+const puzzleList = getPuzzleList();
+
 function setupNextPuzzle() {
-    initBoard();
+    if (puzzleList.length == stageCount) return false;
+
+    const puzzle = puzzleList[stageCount];
+
+    setBoard(puzzle.wordList);
+
+    stageCount++;
 }
 
 {
@@ -19,6 +61,8 @@ function setupNextPuzzle() {
     $(document).ready(function() {
         board = document.getElementById('board');
 
-        setupNextPuzzle();
+        if (puzzleList?.length) {
+            setupNextPuzzle();
+        }
     });
 }
